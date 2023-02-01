7ba371daef8c5f4ee85d261758d941b822fee7e6
Derek Smyth <smyth.derek@gmail.com>
2023-01-31 22:46:06 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+const num1 = 31
+const num2 = 2
 
-// Write your code here
+const multiply = num1 * num2
+
+const random = Math.round(Math.random() * 10);
+
+const num3 = 32
+const num4 = 7
+
+const mod = num3 % num4;
+
+const max = Math.max(1, 2, 17, 20, 13);
